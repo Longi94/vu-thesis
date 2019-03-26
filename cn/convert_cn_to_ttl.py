64405b12c08c845af47ca1b7e28704f566ec6896
Long Tran <lngtrn94@gmail.com>
2019-03-26 19:49:42 +0100

    script to convert conceptnet to TTL

---
+++ cn/convert_cn_to_ttl.py
@@ -0,0 +1,16 @@
+import argparse
+
+arg_parser = argparse.ArgumentParser()
+arg_parser.add_argument('--input', '-i', type=str, required=True)
+arg_parser.add_argument('--output', '-o', type=str, required=True)
+args = arg_parser.parse_args()
+
+with open(args.output, 'w', encoding="utf8") as o:
+    with open(args.input, 'r', encoding="utf8") as f:
+        for line in f:
+            line = line[3:line.index('\t')]
+            line = line[1:-1]
+
+            assertion = line.split(',')
+
+            o.write('<{}> <{}> <{}> .\n'.format(assertion[1], assertion[0], assertion[2]))
