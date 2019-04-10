c567e7d5904f872037e4b1e08c15b7059f27ee1f
Long Tran <lngtrn94@gmail.com>
2019-04-10 22:04:00 +0200

    fix up wordnet links

--- cn/convert_cn_to_ttl.py
+++ cn/convert_cn_to_ttl.py
@@ -1,4 +1,5 @@
 import argparse
+import re
 
 arg_parser = argparse.ArgumentParser()
 arg_parser.add_argument('--input', '-i', type=str, required=True)
@@ -7,6 +8,8 @@ args = arg_parser.parse_args()
 
 with open(args.output, 'w', encoding="utf8") as o:
     with open(args.input, 'r', encoding="utf8") as f:
+        wordnet_regex = re.compile(r"/wn31/[0-9]", re.IGNORECASE)
+
         for line in f:
             line = line[3:line.index('\t')]
             line = line[1:-1]
@@ -27,6 +30,6 @@ with open(args.output, 'w', encoding="utf8") as o:
                     assertion[2] = assertion[2][1:]
 
                 if assertion[2].startswith('http://wordnet-rdf.princeton.edu/wn31/'):
-                    assertion[2] = assertion[2].replace('/wn31/3', '/id/')
+                    assertion[2] = wordnet_regex.sub('/id/', assertion[2])
 
             o.write('<{}> <{}> <{}> .\n'.format(assertion[1][:-1], assertion[0][:-1], assertion[2][:-1]))
